c7d11dfcbcd45e97258e597ed097ae367ecb1617
Soumilcoding <69944759+Soumilcoding@users.noreply.github.com>
2020-09-09 19:22:04 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var bullet,speed,weigth;
+
+var wall,thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+  thickness = random(22,83);
+  speed = random(223,321);
+  weight = random(30,52);
+
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80);
+
+  bullet = createSprite(200,200,50,20);
+
+}
+
+function draw() {
+  background("black");
+  bullet.velocityX =  speed;
+
+if(hasCollided(bullet,wall)){
+bullet.velocityX = 0;
+var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+}
+
+if (damage>10){
+wall.shapeColor = color(255,0,0);
+}
+
+if(damage<10){
+wall.shapeColoe = color(0,255,0)
+}
+
+
+  drawSprites();
+}
+
+function hasCollided(bullet,wall){
+
+bulletRightEdge=bullet.x + bullet.width;
+wallLeftEdge = wall.x;
+if(bulletRightEdge>=wallLeftEdge){
+return true;
+}   
+{
+return false;
+}
+
+}
+
